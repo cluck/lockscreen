70ba6d7db05a1fcbb0551d4765ac7046e146daa0
Claudio Luck <claudio.luck@pep.foundation>
2018-04-16 17:37:47 +0200

    try to fix freeze on wakeup
    loop until the OS notifies screen is locked

--- Makefile
+++ Makefile
@@ -1,8 +1,9 @@
 
 all: lockscreen
 
-lockscreen:
+lockscreen: lockscreen.c
 	clang -F /System/Library/PrivateFrameworks \
 	    -framework login -framework IOKit -framework Foundation \
+		-framework ApplicationServices \
 	    -mmacosx-version-min=10.6 \
 	    -o lockscreen lockscreen.c
--- lockscreen.c
+++ lockscreen.c
@@ -1,23 +1,74 @@
 // lockscreen.c
 
-#include <unistd.h>
+#include <ApplicationServices/ApplicationServices.h>
 #include <CoreFoundation/CoreFoundation.h>
-#include <IOKit/IOKitLib.h>
+#include <IOKit/IOMessage.h>
+
+// #include <unistd.h>
 
 
 extern void SACLockScreenImmediate ();
 
-int main(int argc, const char * argv[])
+void displayPowerNotificationsCallback (
+    void *refcon,
+    io_service_t service,
+    natural_t messageType,
+    void *messageArgument
+)
 {
+  static bool powerOffSeen = false;
+  switch (messageType) {
+    case kIOMessageDeviceWillPowerOff:
+      if (powerOffSeen)
+        break;
+      powerOffSeen = true;
+      CFRunLoopStop(CFRunLoopGetCurrent());
+      break;
+  }
+}
 
-    SACLockScreenImmediate();
+int main (
+    int argc,
+    const char * argv[]
+)
+{
+  io_service_t displayWrangler;
+  io_object_t notification;
+  IONotificationPortRef notificationPort;
+  CFDictionaryRef sessionInfoDict;
+  CFBooleanRef sessionIsLocked;
+  bool result = false;
 
-	io_registry_entry_t entry = IORegistryEntryFromPath(
-			kIOMasterPortDefault,
-			"IOService:/IOResources/IODisplayWrangler"
-		);
-    IORegistryEntrySetCFProperty(entry, CFSTR("IORequestIdle"), kCFBooleanTrue);
-    IOObjectRelease(entry);
+  displayWrangler = IOServiceGetMatchingService (kIOMasterPortDefault,
+                                                 IOServiceNameMatching ("IODisplayWrangler"));
 
-    return 0;
+  notificationPort = IONotificationPortCreate (kIOMasterPortDefault);
+
+  IOServiceAddInterestNotification (notificationPort, displayWrangler, kIOGeneralInterest,
+                                    displayPowerNotificationsCallback, NULL, &notification);
+
+  CFRunLoopAddSource (CFRunLoopGetCurrent (),
+                      IONotificationPortGetRunLoopSource (notificationPort),
+                      kCFRunLoopDefaultMode);
+
+  SACLockScreenImmediate ();
+
+  
+  while (!result)
+  {
+    sessionInfoDict = CGSessionCopyCurrentDictionary();
+    sessionIsLocked = CFDictionaryGetValue (sessionInfoDict,
+                                            CFSTR("CGSSessionScreenIsLocked"));
+    if (sessionIsLocked) {
+      result = CFBooleanGetValue (sessionIsLocked);
+    }
+    CFRelease (sessionInfoDict);
+    usleep(100000);
+  }
+
+  IORegistryEntrySetCFProperty (displayWrangler, CFSTR("IORequestIdle"), kCFBooleanTrue);
+  IOObjectRelease (displayWrangler);
+  CFRunLoopRun ();
+
+  return 0;
 }
